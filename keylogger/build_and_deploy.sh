421c69cb24f4211a897ce56ae9120a33389c9be6
Nathan Germond <germond.nathan@gmail.com>
2025-03-12 16:22:06 +0100

    moving files

--- keylogger/build_and_deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-PROGRAM_NAME="keylogger"
-WINDOWS_DESKTOP="/mnt/c/Users/germo/Desktop"
-cargo build --release --target x86_64-pc-windows-gnu
-cp target/x86_64-pc-windows-gnu/release/$PROGRAM_NAME.exe $WINDOWS_DESKTOP/
-echo "✅ Compilation terminée ! Fichier copié sur le Bureau Windows."
